7bd810d4fc187d138f03c640b1899b25092cd7ee
kalikaloo <pragyagurung002@gmail.com>
2020-11-16 15:59:16 +0000

    stuff

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,6 +1,9 @@
 package gol
 
 import (
+	"strconv"
+	"strings"
+
 	"uk.ac.bris.cs/gameoflife/util"
 )
 
@@ -8,17 +11,28 @@ type distributorChannels struct {
 	events    chan<- Event
 	ioCommand chan<- ioCommand
 	ioIdle    <-chan bool
+	filename  chan<- string
+	input     <-chan uint8
 }
 
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
 
-	// TODO: Create a 2D slice to store the world.
+	c.ioCommand <- ioInput
+	c.filename <- strings.Join([]string{strconv.Itoa(p.ImageWidth), strconv.Itoa(p.ImageHeight)}, "x")
 
+	// TODO: Create a 2D slice to store the world.
 	world := make([][]byte, p.ImageHeight)
 	for i := range world {
 		world[i] = make([]byte, p.ImageWidth)
 	}
+
+	for i := range world {
+		for j := range world {
+			world[i][j] = <-c.input
+		}
+	}
+
 	// TODO: For all initially alive cells send a CellFlipped Event.
 	for y := 0; y < p.ImageHeight; y++ {
 		for x := 0; x < p.ImageWidth; x++ {
@@ -31,10 +45,14 @@ func distributor(p Params, c distributorChannels) {
 	turn := 0
 	for turn = 0; turn < p.Turns; turn++ {
 		world = calculateNextState(p, world)
+		c.events <- TurnComplete{CompletedTurns: turn}
 	}
 
 	// TODO: Send correct Events when required, e.g. CellFlipped, TurnComplete and FinalTurnComplete.
 	//		 See event.go for a list of all events.
+
+	c.ioCommand <- ioOutput
+	c.filename <- strings.Join([]string{strconv.Itoa(p.ImageWidth), strconv.Itoa(p.ImageHeight)}, "x")
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
--- gol/gol.go
+++ gol/gol.go
@@ -1,4 +1,8 @@
 package gol
+import (
+	"strconv"
+	"strings"
+)
 
 // Params provides the details of how to run the Game of Life and which image to load.
 type Params struct {
@@ -87,23 +91,29 @@ func calculateAliveCells(p Params, world [][]byte) []cell {
 
 // Run starts the processing of Game of Life. It should initialise channels and goroutines.
 func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
-
+	
 	ioCommand := make(chan ioCommand)
 	ioIdle := make(chan bool)
+	filename := make (chan string)
+	input := make(chan uint8)
+	output := make(chan uint8)
+	
 
 	distributorChannels := distributorChannels{
 		events,
 		ioCommand,
 		ioIdle,
+		filename,
+		input,
 	}
 	go distributor(p, distributorChannels)
 
 	ioChannels := ioChannels{
 		command:  ioCommand,
 		idle:     ioIdle,
-		filename: nil,
-		output:   nil,
-		input:    nil,
+		filename: filename,
+		output:   output,
+		input:    input,
 	}
 	go startIo(p, ioChannels)
 }
