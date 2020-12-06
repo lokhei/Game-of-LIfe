299612fc08d118d524472ea2de7341ef2ec6de5d
lokhei <lokhei01@gmail.com>
2020-12-06 15:41:53 +0000

    add halo file

---
+++ Distributed-halo/stubs/stubs.go
@@ -0,0 +1,41 @@
+package stubs
+
+var CallInitial = "NextStateOperation.InitialState"
+var CallReturn = "NextStateOperation.FinalState"
+var CallAlive = "NextStateOperation.Alive"
+var CallDoKeypresses = "NextStateOperation.DoKeypresses"
+var CalculateNextState = "Worker.CalculateNextState"
+var GetAddress = "NextStateOperation.GetAddress"
+
+type Response struct {
+	AliveCells int
+	Message    [][]byte
+	Turn       int
+	Done       bool
+}
+
+type Request struct {
+	Message  [][]byte
+	Threads  int
+	Turns    int
+	Keypress rune
+	Pause    bool
+}
+
+type ReqWorker struct {
+	World  [][]byte
+	StartY int
+	EndY   int
+}
+
+type ResWorker struct {
+	World [][]byte
+}
+
+type ReqAddress struct {
+	WorkerAddress string
+}
+
+type ResAddress struct {
+	// ErrorMessage string
+}
