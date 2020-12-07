80b58ebd1bd5c2937f52cbfecfbea723e0b8235c
kalikaloo <pragyagurung002@gmail.com>
2020-12-07 11:24:42 +0000

    benchmark testing splidthreads nice

--- Concurrent/benchmark_test.go
+++ Concurrent/benchmark_test.go
@@ -9,16 +9,52 @@ import (
 )
 
 const turnNum = 1000
+
 var value bool
 
 func Benchmark(b *testing.B) {
 	tests := []gol.Params{
-		{ImageWidth: 512, ImageHeight: 512, Threads: 1},
-		{ImageWidth: 512, ImageHeight: 512, Threads: 5},
+
+		{ImageWidth: 16, ImageHeight: 16, Threads: 2},
+		{ImageWidth: 16, ImageHeight: 16, Threads: 4},
+		{ImageWidth: 16, ImageHeight: 16, Threads: 8},
+		{ImageWidth: 16, ImageHeight: 16, Threads: 10},
+		{ImageWidth: 16, ImageHeight: 16, Threads: 12},
+		{ImageWidth: 16, ImageHeight: 16, Threads: 14},
+		{ImageWidth: 16, ImageHeight: 16, Threads: 16},
+
+		{ImageWidth: 64, ImageHeight: 64, Threads: 2},
+		{ImageWidth: 64, ImageHeight: 64, Threads: 4},
+		{ImageWidth: 64, ImageHeight: 64, Threads: 8},
+		{ImageWidth: 64, ImageHeight: 64, Threads: 10},
+		{ImageWidth: 64, ImageHeight: 64, Threads: 12},
+		{ImageWidth: 64, ImageHeight: 64, Threads: 14},
+		{ImageWidth: 64, ImageHeight: 64, Threads: 16},
+
+		{ImageWidth: 128, ImageHeight: 128, Threads: 2},
+		{ImageWidth: 128, ImageHeight: 128, Threads: 4},
+		{ImageWidth: 128, ImageHeight: 128, Threads: 8},
+		{ImageWidth: 128, ImageHeight: 128, Threads: 10},
+		{ImageWidth: 128, ImageHeight: 128, Threads: 12},
+		{ImageWidth: 128, ImageHeight: 128, Threads: 14},
+		{ImageWidth: 128, ImageHeight: 128, Threads: 16},
+
+		{ImageWidth: 256, ImageHeight: 256, Threads: 2},
+		{ImageWidth: 256, ImageHeight: 256, Threads: 4},
+		{ImageWidth: 256, ImageHeight: 256, Threads: 8},
+		{ImageWidth: 256, ImageHeight: 256, Threads: 10},
+		{ImageWidth: 256, ImageHeight: 256, Threads: 12},
+		{ImageWidth: 256, ImageHeight: 256, Threads: 14},
+		{ImageWidth: 256, ImageHeight: 256, Threads: 16},
+
+		{ImageWidth: 512, ImageHeight: 512, Threads: 2},
+		{ImageWidth: 512, ImageHeight: 512, Threads: 4},
+		{ImageWidth: 512, ImageHeight: 512, Threads: 8},
 		{ImageWidth: 512, ImageHeight: 512, Threads: 10},
+		{ImageWidth: 512, ImageHeight: 512, Threads: 12},
+		{ImageWidth: 512, ImageHeight: 512, Threads: 14},
 		{ImageWidth: 512, ImageHeight: 512, Threads: 16},
 	}
-
 	for _, t := range tests {
 		value = true
 		os.Stdout = nil
@@ -46,44 +82,3 @@ func Benchmark(b *testing.B) {
 		})
 	}
 }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"testing"
-
-// 	"uk.ac.bris.cs/gameoflife/gol"
-// )
-
-// // TestGol tests 16x16, 64x64 and 512x512 images on 0, 1 and 100 turns using 1-16 worker threads.
-// func BenchmarkTests(b *testing.B) {
-// 	tests := []gol.Params{
-// 		{ImageWidth: 16, ImageHeight: 16},
-// 		{ImageWidth: 64, ImageHeight: 64},
-// 		{ImageWidth: 512, ImageHeight: 512},
-// 	}
-// 	for _, p := range tests {
-// 		for _, turns := range []int{0, 1, 100} {
-// 			p.Turns = turns
-// 			for threads := 1; threads <= 16; threads++ {
-// 				p.Threads = threads
-// 				testName := fmt.Sprintf("%dx%dx%d-%d", p.ImageWidth, p.ImageHeight, p.Turns, p.Threads)
-// 				b.Run(testName, func(b *testing.B) {
-// 					for i := 0; i < b.N; i++ {
-// 						events := make(chan gol.Event)
-// 						gol.Run(p, events, nil)
-// 						// var cells []util.Cell
-// 						for event := range events {
-// 							switch e := event.(type) {
-// 							case gol.FinalTurnComplete:
-// 								cells := e.Alive
-// 								break
-// 							}
-// 						}
-// 					}
-// 				})
-// 			}
-// 		}
-// 	}
-// }
