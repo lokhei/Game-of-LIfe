be874f354af46bebb97866fb26d2b2beeb28dcfa
kalikaloo <pragyagurung002@gmail.com>
2020-12-09 17:44:27 +0000

    benchmark redo 512 all threads

--- Concurrent/benchmark_test.go
+++ Concurrent/benchmark_test.go
@@ -13,81 +13,23 @@ const turnNum = 1000
 var value bool
 
 func Benchmark(b *testing.B) {
-	tests := []gol.Params{
+	os.Stdout = nil
 
-		{ImageWidth: 16, ImageHeight: 16, Threads: 2},
-		{ImageWidth: 16, ImageHeight: 16, Threads: 4},
-		{ImageWidth: 16, ImageHeight: 16, Threads: 6},
-		{ImageWidth: 16, ImageHeight: 16, Threads: 8},
-		{ImageWidth: 16, ImageHeight: 16, Threads: 10},
-		{ImageWidth: 16, ImageHeight: 16, Threads: 12},
-		{ImageWidth: 16, ImageHeight: 16, Threads: 14},
-		{ImageWidth: 16, ImageHeight: 16, Threads: 16},
-
-		{ImageWidth: 64, ImageHeight: 64, Threads: 2},
-		{ImageWidth: 64, ImageHeight: 64, Threads: 4},
-		{ImageWidth: 64, ImageHeight: 64, Threads: 6},
-		{ImageWidth: 64, ImageHeight: 64, Threads: 8},
-		{ImageWidth: 64, ImageHeight: 64, Threads: 10},
-		{ImageWidth: 64, ImageHeight: 64, Threads: 12},
-		{ImageWidth: 64, ImageHeight: 64, Threads: 14},
-		{ImageWidth: 64, ImageHeight: 64, Threads: 16},
-
-		{ImageWidth: 128, ImageHeight: 128, Threads: 2},
-		{ImageWidth: 128, ImageHeight: 128, Threads: 4},
-		{ImageWidth: 128, ImageHeight: 128, Threads: 6},
-		{ImageWidth: 128, ImageHeight: 128, Threads: 8},
-		{ImageWidth: 128, ImageHeight: 128, Threads: 10},
-		{ImageWidth: 128, ImageHeight: 128, Threads: 12},
-		{ImageWidth: 128, ImageHeight: 128, Threads: 14},
-		{ImageWidth: 128, ImageHeight: 128, Threads: 16},
-
-		{ImageWidth: 256, ImageHeight: 256, Threads: 2},
-		{ImageWidth: 256, ImageHeight: 256, Threads: 4},
-		{ImageWidth: 256, ImageHeight: 256, Threads: 6},
-		{ImageWidth: 256, ImageHeight: 256, Threads: 8},
-		{ImageWidth: 256, ImageHeight: 256, Threads: 10},
-		{ImageWidth: 256, ImageHeight: 256, Threads: 12},
-		{ImageWidth: 256, ImageHeight: 256, Threads: 14},
-		{ImageWidth: 256, ImageHeight: 256, Threads: 16},
-
-		{ImageWidth: 512, ImageHeight: 512, Threads: 2},
-		{ImageWidth: 512, ImageHeight: 512, Threads: 4},
-		{ImageWidth: 512, ImageHeight: 512, Threads: 6},
-		{ImageWidth: 512, ImageHeight: 512, Threads: 8},
-		{ImageWidth: 512, ImageHeight: 512, Threads: 10},
-		{ImageWidth: 512, ImageHeight: 512, Threads: 12},
-		{ImageWidth: 512, ImageHeight: 512, Threads: 14},
-		{ImageWidth: 512, ImageHeight: 512, Threads: 16},
-
-		// {ImageWidth: 512, ImageHeight: 512, Threads: 1},
-		// {ImageWidth: 512, ImageHeight: 512, Threads: 2},
-		// {ImageWidth: 512, ImageHeight: 512, Threads: 3},
-		// {ImageWidth: 512, ImageHeight: 512, Threads: 4},
-		// {ImageWidth: 512, ImageHeight: 512, Threads: 5},
-		// {ImageWidth: 512, ImageHeight: 512, Threads: 6},
-		// {ImageWidth: 512, ImageHeight: 512, Threads: 7},
-		// {ImageWidth: 512, ImageHeight: 512, Threads: 8},
-		// {ImageWidth: 512, ImageHeight: 512, Threads: 9},
-		// {ImageWidth: 512, ImageHeight: 512, Threads: 10},
-		// {ImageWidth: 512, ImageHeight: 512, Threads: 11},
-		// {ImageWidth: 512, ImageHeight: 512, Threads: 12},
-		// {ImageWidth: 512, ImageHeight: 512, Threads: 13},
-		// {ImageWidth: 512, ImageHeight: 512, Threads: 14},
-		// {ImageWidth: 512, ImageHeight: 512, Threads: 15},
-		// {ImageWidth: 512, ImageHeight: 512, Threads: 16},
-	}
-	for _, t := range tests {
+	for t:=1; t <= 16 ; t++ {
 		value = true
-		os.Stdout = nil
-		t.Turns = turnNum
-		name := fmt.Sprintf("%dx%dx%d-%d", t.ImageWidth, t.ImageHeight, turnNum, t.Threads)
+		tests := gol.Params{
+
+			ImageWidth: 512, ImageHeight: 512,
+			
+		}
+		tests.Threads = t
+		name := fmt.Sprintf("%dx%dx%d-%d", tests.ImageWidth, tests.ImageHeight, turnNum, t)
 
 		b.Run(name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				if value == true {
 					events := make(chan gol.Event)
-					gol.Run(t, events, nil)
+					gol.Run(tests, events, nil)
 
 					value = false
 
